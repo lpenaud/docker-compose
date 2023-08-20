ee3bdb2f2c83faa94dc300a173c38775c69e990a
lpenaud <l.penaud@zaclys.net>
2023-08-20 04:13:17 +0200

    FEAT: Add Vagrant

---
+++ provision.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Refresh the currents repositories
+apt-get update
+# Upgrade all installed software
+apt-get upgrade -y
+# Install all dependencies from the official repositories
+apt-get install -y gnupg apt-transport-https ca-certificates curl debian-keyring debian-archive-keyring vim
+
+# Add Docker APT repository 
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL "https://download.docker.com/linux/debian/gpg" | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" > /etc/apt/sources.list.d/docker.list
+
+# Add Caddy repository
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+
+# Refresh of all new repositories
+apt-get update
+# Install all software from the new repositories
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-ce-rootless-extras docker-buildx-plugin docker-compose caddy
