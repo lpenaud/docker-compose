97bcf7d106c9975cde49ac0b73a307fc0dda2b89
Loïc Penaud <l.penaud@zaclys.net>
2024-04-05 01:43:20 +0200

    FEAT(torrent): Update default-trackers

--- torrent/default-trackers.sh
+++ torrent/default-trackers.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+declare -r FILENAME="$(realpath "${BASH_SOURCE[0]}")"
+declare -r DIRNAME="$(dirname "${FILENAME}")"
+
 if [ $# -ne 2 ]; then
   printf "Usage: %s URL SETTINGS\n" "${BASH_SOURCE[0]}"
   exit 1
@@ -7,4 +10,6 @@ fi
 
 declare -r BAK="${2}.bak"
 cp "${2}" "${BAK}"
+docker compose --project-directory "${DIRNAME}" down
 jq -M ".[\"default-trackers\"] |= \"$(curl "${1}")\"" < "${BAK}" > "${2}"
+docker compose --project-directory "${DIRNAME}" up -d
