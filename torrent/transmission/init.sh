82989577850aea3e507670432396de5296e430b4
Loïc Penaud <l.penaud@zaclys.net>
2024-06-10 02:13:50 +0200

    FIX(torrent): Peer port is a number

--- torrent/transmission/init.sh
+++ torrent/transmission/init.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
 declare -r settings="$(cat /config/settings.json)"
-declare -a values
+declare jq_expr
 
-# NAME VALUE
-function jq_expr () {
-  if [ $# -lt 2 ]; then
-    return 0
-  fi
-  printf '.["%s"]="%s"' "${1}" "${2}"
-  while shift 2 && [ $# -ge 2 ]; do
-    printf ' | .["%s"]="%s"' "${1}" "${2}"
-  done
-  return 0
-}
-
-values+=("peer-port" "$(cat /protonvpn-port/protonvpn-port)")
+jq_expr=".[\"peer-port\"]=$(cat /protonvpn-port/protonvpn-port)"
 if [ -n "${TRACKERS_URL}" ]; then
+  jq_expr+=" | .[\"default-trackers\"]=\"$(curl "${TRACKERS_URL}")\""
   values+=("default-trackers" "$(curl "${TRACKERS_URL}")")
 fi
 
-echo "${settings}" | jq "$(jq_expr "${values[@]}")" > /config/settings.json
+echo "${settings}" | jq "${jq_expr}" > /config/settings.json
 
