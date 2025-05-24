0459afbc39d77096d8519e07f4e73464c38df949
lpenaud <l.penaud@zaclys.net>
2025-05-24 18:34:46 +0200

    FEAT: Use linuxserver/wireguard

--- torrent/protonvpn/protonvpn-healthcheck.sh
+++ torrent/protonvpn/protonvpn-healthcheck.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # Check if all is ok
-protonwire verify "${PROTONVPN_SERVER}" && \
-  natpmpc -a 1 0 udp 60 -g 10.2.0.1 && \
+natpmpc -a 1 0 udp 60 -g 10.2.0.1 && \
   natpmpc -a 1 0 tcp 60 -g 10.2.0.1
