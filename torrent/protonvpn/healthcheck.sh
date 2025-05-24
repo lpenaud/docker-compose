c44aa65399b5f39ba98b45cb77b214f51df642fa
lpenaud <l.penaud@zaclys.net>
2025-05-24 20:23:48 +0200

    FIX: Don't forget the anti-slash

--- torrent/protonvpn/healthcheck.sh
+++ torrent/protonvpn/healthcheck.sh
@@ -14,5 +14,5 @@ fi
 # Run port forwarding
 # Write the port to /config/protonvpn-port
 natpmpc -a 1 0 udp 60 -g 10.2.0.1 \
-  && natpmpc -a 1 0 tcp 60 -g 10.2.0.1
+  && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 \
   | grep -m 1 -oP 'public\ port\ \K\w+' > /protonvpn-port/protonvpn-port
