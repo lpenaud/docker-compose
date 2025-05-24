bcc6be9d3cd0bc6397fc7b93ba0b7d767073d32a
lpenaud <l.penaud@zaclys.net>
2025-05-24 21:15:22 +0200

    FIX(torrent): VPN scripts

--- torrent/protonvpn/custom-services.d/init-protonvpn-port.sh
+++ torrent/protonvpn/custom-services.d/init-protonvpn-port.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Run port forwarding
-# Write the port to /config/protonvpn-port
-natpmpc -a 1 0 udp 60 -g 10.2.0.1 \
-  && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 \
-  | grep -m 1 -oP 'public\ port\ \K\w+' > /protonvpn-port/protonvpn-port
+rm -vf "/protonvpn-port/protonvpn-port"
