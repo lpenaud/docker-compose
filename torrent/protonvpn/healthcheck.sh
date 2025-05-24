631efa25973e962f412817e9335f398cb050d9bd
lpenaud <l.penaud@zaclys.net>
2025-05-24 20:11:20 +0200

    FIX: Init

--- torrent/protonvpn/healthcheck.sh
+++ torrent/protonvpn/healthcheck.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-declare -r PROTON_PORT="/protonvpn-port/protonvpn-port"
-
-rm -f "${PROTON_PORT}"
-
 if ! wg show | grep -m 1 -q "${PROTONVPN_SERVER}"; then
   exit 1
 fi
 
 # Maintains port forwarding
-if [ -f "${PROTON_PORT}" ]; then
+if [ -f "/protonvpn-port/protonvpn-port" ]; then
   natpmpc -a 1 0 udp 60 -g 10.2.0.1 \
     && natpmpc -a 1 0 tcp 60 -g 10.2.0.1
   exit
@@ -20,4 +16,3 @@ fi
 natpmpc -a 1 0 udp 60 -g 10.2.0.1 \
   && natpmpc -a 1 0 tcp 60 -g 10.2.0.1
   | grep -m 1 -oP 'public\ port\ \K\w+' > /protonvpn-port/protonvpn-port
-
