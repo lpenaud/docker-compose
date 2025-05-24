0450ca9cb664c6d79db0522db08beca6e162dd27
lpenaud <l.penaud@zaclys.net>
2025-05-24 21:11:42 +0200

    FIX(torrent): VPN scripts

--- torrent/protonvpn/custom-services.d/init-protonvpn-port.sh
+++ torrent/protonvpn/custom-services.d/init-protonvpn-port.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-rm -vf "/protonvpn-port/protonvpn-port"
+# Run port forwarding
+# Write the port to /config/protonvpn-port
+natpmpc -a 1 0 udp 60 -g 10.2.0.1 \
+  && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 \
+  | grep -m 1 -oP 'public\ port\ \K\w+' > /protonvpn-port/protonvpn-port
--- torrent/protonvpn/healthcheck.sh
+++ torrent/protonvpn/healthcheck.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-if ! wg show | grep -m 1 -q "${PROTONVPN_SERVER}"; then
-  exit 1
-fi
-
-# Maintains port forwarding
-if [ -f "/protonvpn-port/protonvpn-port" ]; then
-  natpmpc -a 1 0 udp 60 -g 10.2.0.1 \
-    && natpmpc -a 1 0 tcp 60 -g 10.2.0.1
-  exit
-fi
-
-# Run port forwarding
-# Write the port to /config/protonvpn-port
 natpmpc -a 1 0 udp 60 -g 10.2.0.1 \
-  && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 \
-  | grep -m 1 -oP 'public\ port\ \K\w+' > /protonvpn-port/protonvpn-port
+  && natpmpc -a 1 0 tcp 60 -g 10.2.0.1
