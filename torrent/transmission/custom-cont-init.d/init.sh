f6285a0178025ed19193814cbcca25da024a65ca
Loïc Penaud <l.penaud@zaclys.net>
2025-06-01 18:00:06 +0200

    FEAT(torrent): Add script-torrent-done-filename (#4)

--- torrent/transmission/custom-cont-init.d/init.sh
+++ torrent/transmission/custom-cont-init.d/init.sh
@@ -7,6 +7,12 @@ jq_expr=".[\"peer-port\"]=$(cat /protonvpn-port/protonvpn-port)"
 if [ -n "${TRACKERS_URL}" ]; then
   jq_expr+=" | .[\"default-trackers\"]=\"$(curl "${TRACKERS_URL}")\""
 fi
+if [ -n "${TORRENT_DONE}" ]; then
+  jq_expr+=" | .[\"script-torrent-done-enabled\"]=true"
+  jq_expr+=" | .[\"script-torrent-done-filename\"]=\"${TORRENT_DONE}\""
+else
+  jq_expr+=" | .[\"script-torrent-done-enabled\"]=false"
+fi
 
 echo "${settings}" | jq "${jq_expr}" > /config/settings.json
 
