a0c0291c7e94019df2f02866133bc739c9861173
Loïc Penaud <l.penaud@zaclys.net>
2025-06-01 21:56:26 +0200

    FIX(torrent): Make done dir

--- torrent/transmission/custom-cont-init.d/script-torrent-done.sh
+++ torrent/transmission/custom-cont-init.d/script-torrent-done.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Not calling by transmission then pass
+# Init calling
 if [ -z "${TR_TORRENT_HASH}" ]; then
-  exit 0
+  mkdir -p "/downloads/done"
+  exit
 fi
 
+# Transmission after done torrent calling
 printf '{"labels":"%s","name":"%s"}' \
   "${TR_TORRENT_LABELS}" \
   "${TR_TORRENT_NAME}" > "/downloads/done/${TR_TORRENT_HASH}.json"  
