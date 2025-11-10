6df732746bb1c43fe9320eb529ecb875ed02e45e
lpenaud <l.penaud@zaclys.net>
2025-11-10 02:45:32 +0100

    FIX(torrent): Remove newline char

--- torrent/transmission/custom-cont-init.d/script-torrent-done.sh
+++ torrent/transmission/custom-cont-init.d/script-torrent-done.sh
@@ -6,19 +6,26 @@ if [ -z "${TR_TORRENT_HASH}" ]; then
   exit
 fi
 
+# value
+# Sometime there are value with newline T_T
+# So we need this little fix.
+function json::format () {
+  printf "%s" "${1}"
+}
+
 # Transmission after done torrent calling
 # Variables: https://github.com/transmission/transmission/blob/main/docs/Scripts.md#on-torrent-completion
 cat <<EOF > "/downloads/done/${TR_TORRENT_NAME}.${TR_TORRENT_HASH}.json"
 {
-  "version": "${TR_APP_VERSION}",
-  "time": "${TR_TIME_LOCALTIME}",
-  "downloaded": "${TR_TORRENT_BYTES_DOWNLOADED}",
-  "dir": "${TR_TORRENT_DIR}",
-  "hash": "${TR_TORRENT_HASH}",
-  "id": "${TR_TORRENT_ID}",
-  "labels": "${TR_TORRENT_LABELS}",
-  "name": "${TR_TORRENT_NAME}",
-  "priority": "${TR_TORRENT_PRIORITY}",
-  "trackers": "${TR_TORRENT_TRACKERS}"
+  "version": "$(json::format "${TR_APP_VERSION}")",
+  "time": "$(json::format "${TR_TIME_LOCALTIME}")",
+  "downloaded": "$(json::format "${TR_TORRENT_BYTES_DOWNLOADED}")",
+  "dir": "$(json::format "${TR_TORRENT_DIR}")",
+  "hash": "$(json::format "${TR_TORRENT_HASH}")",
+  "id": "$(json::format "${TR_TORRENT_ID}")",
+  "labels": "$(json::format "${TR_TORRENT_LABELS}")",
+  "name": "$(json::format "${TR_TORRENT_NAME}")",
+  "priority": "$(json::format "${TR_TORRENT_PRIORITY}")",
+  "trackers": "$(json::format "${TR_TORRENT_TRACKERS}")"
 }
 EOF
