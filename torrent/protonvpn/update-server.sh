b249f5b52b30b60a988038c2442f51118cd6d783
lpenaud <l.penaud@zaclys.net>
2026-05-24 16:59:25 +0200

    WIP: Use gluetun

---
+++ torrent/protonvpn/update-server.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Update proton vpn server list.
+# See: https://github.com/qdm12/gluetun-wiki/blob/main/setup/servers.md
+
+declare -r FILENAME="$(realpath "${0}")"
+declare -r DIRNAME="$(dirname "${FILENAME}")"
+
+declare email password
+
+read -p "Proton email: " email
+read -s -p "Proton password: " password
+docker run --rm -v /yourpath:/gluetun qmcgaw/gluetun \
+  update \
+  -enduser \
+  -providers protonvpn \
+  -proton-email "${email}" \
+  -proton-password "${password}"
